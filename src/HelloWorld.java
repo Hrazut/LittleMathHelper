a3993abdfb12e5607af389c0b9bdcc1f626ca82c
Jan Brandt <jbrandt1@freenet.de>
2016-05-15 20:29:56 +0000

    test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,10 @@
-
+﻿﻿
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Jan macht ne Änderung... lalalala
-		System.out.println("Hello World!");
+		System.out.println("Hello new World!");
 	}
-	//Hrazut
+
 }
