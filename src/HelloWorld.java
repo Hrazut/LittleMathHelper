5b0b2ad178cc819dd9bb7a4cee9015102c2d6a99
Adrein <Adrian.Marosfalvi@gmx.de>
2016-05-15 21:13:02 +0200

    Ich hab nen new reingepackt

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		//Jan macht ne Änderung... lalalala
+		System.out.println("Hello new World!");
 	}
 
 }
