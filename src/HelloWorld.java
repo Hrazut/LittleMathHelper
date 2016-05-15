5c7e98c0fa3510afe3b6231c8ff6a12de018ae48
hrazut <tod.space@gmx.net>
2016-05-15 20:59:23 +0200

    Änderung rückgängig
    Test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Jan macht ne Ã„nderung... lalalala
+		//Hrazut machts rückgängig
 		System.out.println("Hello World!");
 	}
 
