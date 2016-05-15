11fe007244aa95cb685a85e86c72f45319cf420d
Jan Brandt <jbrandt1@freenet.de>
2016-05-15 18:42:28 +0000

    Jan ändert was testweise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Jan macht ne Änderung... lalalala
 		System.out.println("Hello World!");
 	}
 
